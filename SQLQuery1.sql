ffb80f6e40d0b5d629c58ff46089966deb1710a5
Shrikant <sawantshrikant07@gmail.com>
2023-07-14 11:55:17 +0530

    [Shrikant] add:CreateTable

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,2 +1,11 @@
 ------UC1 Create Database----------
 create database PayRollServiceDB
+use PayRollServiceDB
+-------UC2 Create Table ---------
+Create table EmployeePayRoll
+(
+EmployeeId int Identity(1,1) primary key,
+Name varchar(255),
+Salary float,
+StartDate date
+)
