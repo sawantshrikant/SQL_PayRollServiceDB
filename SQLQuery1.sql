4eb09acf894af9ab9c61c573c20a93ee3a72217e
Shrikant <sawantshrikant07@gmail.com>
2023-07-26 10:49:25 +0530

    [Shrikant] add:FetchDataForEpmployeeDateRange

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -15,3 +15,10 @@ insert into EmployeePayRollDataBase (Name,StartDate) values ('Dhoni',GETDATE())
 -----UC4 fetch data from table ---- 
 select * from EmployeePayRollDataBase
 select Name,Salary from EmployeePayRollDataBase where Name='UV'
+----- UC5 ----------
+SELECT * FROM EmployeePayRollDataBase where Name= 'UV'
+------UPDATE Query-------
+update EmployeePayRollDataBase set StartDate='03-05-2019', Salary=20000 where Name ='Dhoni'
+insert into EmployeePayRollDataBase values('Ashwin',77000,'09-20-2000')
+-------UC5 fetch specify employee date and fetch all employee based date range-----
+select * From EmployeePayRollDataBase WHERE StartDate BETWEEN '2019-01-01' and GETDATE()
