bfd62cfbcc6c420b2e729f36621e7405bb5a9ac1
Shrikant <sawantshrikant07@gmail.com>
2023-07-26 09:35:23 +0530

    [Shrikant] add:FetchDataFromTable

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,7 +2,7 @@
 create database PayRollServiceDB
 use PayRollServiceDB
 -------UC2 Create Table ---------
-Create table EmployeePayRoll
+Create table EmployeePayRollDataBase
 (
 EmployeeId int Identity(1,1) primary key,
 Name varchar(255),
@@ -10,5 +10,8 @@ Salary float,
 StartDate date
 )
 ----UC3 Insert Query-----
-insert into EmployeePayRoll values('UV',87000,GETDATE())
-insert into EmployeePayRoll (Name,StartDate) values ('Dhoni',GETDATE())
+insert into EmployeePayRollDataBase values('UV',87000,GETDATE())
+insert into EmployeePayRollDataBase (Name,StartDate) values ('Dhoni',GETDATE())
+-----UC4 fetch data from table ---- 
+select * from EmployeePayRollDataBase
+select Name,Salary from EmployeePayRollDataBase where Name='UV'
